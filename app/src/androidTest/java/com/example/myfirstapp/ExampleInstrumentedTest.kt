e8039798004e955eda24fcca4e64a1f1f722d8af
Vlad Hilko <vlad.hilko@gmail.com>
2020-02-24 00:54:19 +0300

    fix: change name

--- app/src/androidTest/java/com/example/myfirstapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/myfirstapp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.myfirstapp", appContext.packageName)
+        assertEquals("com.example.FirstRandomApp", appContext.packageName)
     }
 }
